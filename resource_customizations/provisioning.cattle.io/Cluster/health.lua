ee4da83bed0793b81c8f2ea4d2c67ef4ad3cce92
Mikael Sennerholm <mikael@sennerholm.enet>
2023-02-02 10:10:55 +0100

    Initial code

---
+++ resource_customizations/provisioning.cattle.io/Cluster/health.lua
@@ -0,0 +1,34 @@
+function getUpdatedContitionStatus(obj, hs)
+    if obj.status ~= nil and obj.status.conditions ~= nil then
+        for i, condition in ipairs(obj.status.conditions) do
+        if condition.type == "Updated" and condition.status != "True" then
+            hs.status = "Progressing"
+            hs.message = condition.reason + condition.message
+            return hs
+        end
+        end
+    end
+    return hs
+end
+
+
+function getReadyContitionStatus(obj, hs)
+
+    if obj.status ~= nil and obj.status.conditions ~= nil then
+        for i, condition in ipairs(obj.status.conditions) do
+        if condition.type == "Ready" and condition.status == "False" then
+            hs.status = "Degraded"
+            hs.message = condition.message
+            return hs
+        end
+        end
+    end
+    return hs
+end
+hs = {}
+hs.status = "Healthy"
+hs.message = "Cluster is running"
+getUpdatedContitionStatus(obj, hs)
+getReadyContitionStatus(obj, hs)
+
+return hs
